1788a7a03684370a664ba6e615074354d146a945
Niko <nikolaos.papadopoulos@univie.ac.at>
2024-11-13 06:36:19 +0100

    added exercise description

--- rosalind/ini2.py
+++ rosalind/ini2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+# Given: Two positive integers a and b, each less than 1000.
 a = 799
 b = 995
 
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
 result = a**2 + b**2
 print(result)
