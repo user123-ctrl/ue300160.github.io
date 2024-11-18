37e2a38544df3de50efef3d7805be96ed013250d
Niko <nikolaos.papadopoulos@univie.ac.at>
2024-11-18 13:00:14 +0100

    updating INI5 to use read_input

--- rosalind/ini5.py
+++ rosalind/ini5.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+
+from util import read_input
 
 # first, read the file!
-filepath = "../rosalind_data/ini5.txt"
-with open(filepath, 'r') as infile:
-    lines = infile.readlines()
-    for line in lines:
-        print(line.strip())
+test = "../rosalind_data/ini5.txt"
+lines = read_input(test)
+# with open(filepath, 'r') as infile:
+#     lines = infile.readlines()
+#     for line in lines:
+#         print(line.strip())
 
 for l in lines[1::2]:
     print(l.strip())
