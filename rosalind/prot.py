39a3bc47a122c3d59e03596a40fc4c7f9b2a22cf
Niko <nikolaos.papadopoulos@univie.ac.at>
2024-11-22 11:23:16 +0100

    solved translation

---
+++ rosalind/prot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# translate an RNA sequence into protein
+
+from util import read_input
+
+# create a reference dictionary with the genetic code
+# read the RNA in triplets
+# map triplet to aminoacid
+
+code = {
+    "UUU": "F",      "CUU": "L",      "AUU": "I",      "GUU": "V",
+    "UUC": "F",      "CUC": "L",      "AUC": "I",      "GUC": "V",
+    "UUA": "L",      "CUA": "L",      "AUA": "I",      "GUA": "V",
+    "UUG": "L",      "CUG": "L",      "AUG": "M",      "GUG": "V",
+    "UCU": "S",      "CCU": "P",      "ACU": "T",      "GCU": "A",
+    "UCC": "S",      "CCC": "P",      "ACC": "T",      "GCC": "A",
+    "UCA": "S",      "CCA": "P",      "ACA": "T",      "GCA": "A",
+    "UCG": "S",      "CCG": "P",      "ACG": "T",      "GCG": "A",
+    "UAU": "Y",      "CAU": "H",      "AAU": "N",      "GAU": "D",
+    "UAC": "Y",      "CAC": "H",      "AAC": "N",      "GAC": "D",
+    "UAA": "Stop",   "CAA": "Q",      "AAA": "K",      "GAA": "E",
+    "UAG": "Stop",   "CAG": "Q",      "AAG": "K",      "GAG": "E",
+    "UGU": "C",      "CGU": "R",      "AGU": "S",      "GGU": "G",
+    "UGC": "C",      "CGC": "R",      "AGC": "S",      "GGC": "G",
+    "UGA": "Stop",   "CGA": "R",      "AGA": "R",      "GGA": "G",
+    "UGG": "W",      "CGG": "R",      "AGG": "R",      "GGG": "G"
+}
+
+line_list = read_input("../rosalind_data/rosalind_prot.txt")
+rna = line_list[0]
+
+# read the rna in triplets:
+peptide = ""
+for start in range(0, len(rna), 3):
+    codon = rna[start:start+3]
+    aminoacid = code[codon]
+    if aminoacid == "Stop":
+        break
+    peptide += aminoacid
+    # print(codon, "corresponds to", aminoacid)
+
+print(peptide)
