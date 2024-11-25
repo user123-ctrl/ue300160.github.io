dbda915b9f7809ebfefd91f4e1970ee668ac8076
Niko <nikolaos.papadopoulos@univie.ac.at>
2024-11-25 16:55:01 +0100

    template for prtm.py

---
+++ rosalind/prtm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# a script that will take an aminoacid sequence as input and print out
+# the molecular weight of the peptide
+
+# Instructions:
+# 1. read the data
+# 2. define the dictionary (key: amino acid one letter code, value: mass in Da)
+# 3. (actual solution):
+#   - define a counter variable, total_weight, and set it to 0
+#   - loop over the input,
+#   - for the current amino acid, take the value from the dictionary, and add it to total_weight
+#   
+#   print total_weight
